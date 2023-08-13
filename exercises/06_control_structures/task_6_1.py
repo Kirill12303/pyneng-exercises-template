de13351c16e1a285d76580240a7799f59de59ac6
Kirill Nikolaev <73769789+Kirill12303@users.noreply.github.com>
2023-08-13 17:10:14 +0600

    Update task_6_1.py

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -13,3 +13,9 @@ MAC-адреса используются в формате XXXX.XXXX.XXXX
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+
+mac_cisco = []
+for i in mac:
+    mac_cisco.append(i.replace(':', '.'))
+
+print(mac_cisco)
